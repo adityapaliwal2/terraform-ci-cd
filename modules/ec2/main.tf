df63f3fb70baec987f9f45d65b28de02b20c249c
adityapaliwal-syvora <aditya.paliwal@syvora.com>
2026-01-20 18:38:12 +0530

    add : include one more ec2

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,3 +4,10 @@ resource "aws_instance" "app" {
   subnet_id            = var.subnet_id
   iam_instance_profile = var.iam_instance_profile
 }
+
+resource "aws_instance" "app2" {
+  ami                  = "ami-0f5ee92e2d63afc18"
+  instance_type        = "t3.micro"
+  subnet_id            = var.subnet_id
+  iam_instance_profile = var.iam_instance_profile
+}
